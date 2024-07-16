ebea40dc37274241c20a54b4f2c05a3db367d822
Muhammad Ahmed Raza <114856489+nitrofatal@users.noreply.github.com>
2024-07-16 12:13:35 +0500

    Add files via upload

---
+++ Javascript/practice.js
@@ -0,0 +1,115 @@
+// // const tinderUser = new  object()
+// // console.log(tinderUser)
+
+// console.log("hello world")
+
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+ 
+// // const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+
+
+// const course = {
+//     coursename : "jv",
+//     coursePrice: 999,
+//     courseInstructor:"Ahmed"
+// }
+
+// const {courseInstructor : instructor} = course
+
+// console.log(instructor
+
+
+
+// );
+
+
+
+// function add(number1, number2){
+//      console.log(number1 + number2);
+
+// }
+
+// add(3, 5)
+
+
+// function add(number1, number2){
+//     let result = (number1 + number2);
+//     return result
+// }
+// const result = add(3, 5)
+// console.log("Result :", result );
+
+
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMessage("Ahmed"));
+
+
+
+// function calculateCarPrice(num1){ 
+//     return num1 
+// }
+
+// console.log(calculateCarPrice(3))
+
+
+// for (let i = 0; i <= 10; i++ ){
+//     const arr = i;
+//     if (arr === 5) {
+//         console.log("5 Is the best number");
+//     }
+//     console.log(arr);
+// }
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop ${i}` );
+     for (let j = 0; j <= 10; j++) {
+        
+    console.log(`Inner loop ${j}` );
+        
+     }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
